77e3d284bcb2cd083d1bfb64c4dee3e3d2a7ef13
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-26 14:43:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,34 @@
-import http from 'http';
+import http from 'node:http';
 import DIP from './lib/DIP.js';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import { readFileSync } from 'fs';
-import BareServer from '@tomphttp/bare-server-node';
+import { createBareServer } from '@tomphttp/bare-server-node';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const httpServer = http.createServer();
 
-var a = new DIP(join(__dirname, 'public', 'dip'));
-a.watch();
+const bareServer = createBareServer('/');
 
-const bare = new BareServer('/bare/', {
-	logErrors: false,
-	localAddress: undefined,
-	maintainer: {
-		email: 'tomphttp@sys32.dev',
-		website: 'https://github.com/tomphttp/',
-	},
-});
-
-const serve = new a.Server(join(__dirname, 'public'), {headers: {"Service-Worker-Allowed": '/'}});
-
-const server = http.createServer();
-
-server.on('request', (request, response) => {
-  
-	request.url = request.url.replace('https://', 'https:/').replace('https:/', 'https://');
-
-	if (bare.shouldRoute(request)) return bare.routeRequest(request, response);
-
-	if (request.url.startsWith('/service/')) {
-		if (0) response.end(`<script>
-if ('serviceWorker' in navigator) {
-var worker = navigator.serviceWorker.register('/sw.js?${Math.round(Math.random()*(899999)+100000)}', {
-scope: '/service',
-}).then(() => {
-location.reload();
-});
-}
-</script>`);
+httpServer.on('request', (req, res) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeRequest(req, res);
 	} else {
-		serve.serve(request, response);
+		res.writeHead(400);
+		res.end('Not found.');
 	}
 });
 
-server.on('upgrade', (req, socket, head) => {
-	if (bare.shouldRoute(req)) return bare.routeUpgrade(req, socket, head);
-	socket.end();
+httpServer.on('upgrade', (req, socket, head) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
 });
 
-server.listen(process.env.PORT || 80);
+httpServer.on('listening', () => {
+	console.log('HTTP server listening');
+});
+
+httpServer.listen({
+	port: 8080,
+});
